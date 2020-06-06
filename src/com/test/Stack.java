456ecc36f50b5a199904358315099d3fae649c4c
sbiswas84 <sbiswascs@outlook.com>
2020-06-07 01:11:13 +0530

    Adding the first program

---
+++ src/com/test/Stack.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Stack {
+ public static void main(String[] args) {
+	
+}
+ 
+ 
+ 
+ 
+
+
+
+
+}
---
+++ src/com/test/Test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+			System.out.println("i:"+i);
+		}
+		System.out.println("Test");
+		System.out.println("Hare Ram...");
+
+	}
+
+}
